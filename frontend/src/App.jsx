74b61383eb206a5e5875d92d89739f03b558e26d
Yasha2583 <adranakleguizamo301@gmail.com>
2025-11-15 21:29:56 -0500

    Creación de Sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Sidebar from './assets'
+import Header from './components/Header'
+import Carrusel from './components/Carrusel'
+import MiColeccion from './components/MiColeccion'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="app">
+      <Sidebar />
+      
+      <div className="contenido-principal">
+        <Header />
+        <Carrusel />
+        <MiColeccion />
       </div>
-      <h1>Nuestro GameTracker</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
---
+++ frontend/src/assets/Sidebar.jsx
@@ -0,0 +1,16 @@
+function Sidebar() {
+  return (
+
+    <div class="sidebar">
+      <div class="Home-icon">🏠</div>
+      <div class="Library-icon">📚</div>
+      <div class="Reseñas-icon">⭐</div>
+      <div class="Estadisticas-icon">📊</div>
+      <div class="Juegos-icon">➕</div>
+      <div class="Settings-icon">⚙️</div>
+    </div>
+
+  )
+}
+
+export default Sidebar
